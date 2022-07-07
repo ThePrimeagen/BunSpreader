19e145d7b20fae2aed3498a88b7110bb4cac3814
mpaulson <mpaulson@netflix.com>
2022-07-07 11:45:07 -0600

    feat: looking sweet babe.

--- src/html.ts
+++ src/html.ts
@@ -1,9 +1,29 @@
+const queue = [];
 
 export default {
     port: 3000,
     async fetch(request: Request) {
         const params = request.url.split("json/")[1];
-        const [time_in_queue] = params.split("/").map(x => +x);
+        let time_in_queue = 5000;
+        if (params) {
+            time_in_queue = params.split("/").map(x => +x)[0];
+        }
+
+        try {
+            const json = await request.json();
+            queue.push({
+                json,
+                time: Date.now() + time_in_queue,
+            });
+        } catch (e) {
+            console.error("unable to parse json", e);
+        }
+
+        const now = Date.now();
+        while (queue.length > 0 && queue[0].time < now) {
+            queue.shift();
+        }
+
         return new Response(`time in queue will be ${time_in_queue}`);
     },
 };
