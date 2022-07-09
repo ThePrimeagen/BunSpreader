4209462fb25e380cdc5d3bb16c0a12765b342c52
mpaulson <mpaulson@netflix.com>
2022-07-09 09:25:12 -0600

    feat: notehuntoehuntoehuntoheuntoheuntoheunthonteuhntoheunth
    first try

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -13,7 +13,7 @@ struct Args {
     #[clap(short = 't', long = "time", default_value= "10000")]
     time_in_queue: usize,
 
-    #[clap(short = 'c', long = "count", default_value= "1000")]
+    #[clap(env = "COUNT", short = 'c', long = "count", default_value= "1000")]
     count: usize,
 
     #[clap(short = 'a', long = "address", default_value= "0.0.0.0")]
@@ -22,7 +22,7 @@ struct Args {
     #[clap(short = 'p', long = "port", default_value = "42069")]
     port: u16,
 
-    #[clap(short = 'm', long = "max_conn", default_value = "100")]
+    #[clap(env = "MAX_CONN", short = 'm', long = "max_conn", default_value = "100")]
     max_conn: usize,
 }
 
@@ -73,14 +73,7 @@ async fn main() -> Result<()> {
     let client = Client::new();
     let stats = Arc::new(Stats::default());
 
-    let max_conn = std::env::var("MAX_CONN")
-        .ok()
-        .as_ref()
-        .and_then(|x| {
-            return str::parse::<usize>(x).ok();
-        })
-        .unwrap_or(args.max_conn);
-    let semaphore = Arc::new(Semaphore::new(max_conn));
+    let semaphore = Arc::new(Semaphore::new(args.max_conn));
 
     let url = format!("http://{}:{}/json/{}", args.address, args.port, args.time_in_queue);
     println!("using this url {}", url);
@@ -101,15 +94,7 @@ async fn main() -> Result<()> {
     };
     let body = serde_json::to_string(&body)?;
 
-    let count = std::env::var("COUNT")
-        .ok()
-        .as_ref()
-        .and_then(|x| {
-            return str::parse::<usize>(x).ok();
-        })
-        .unwrap_or(args.count);
-
-    for i in 0..count {
+    for i in 0..args.count {
         if i % 10 == 0 {
             println!("look at my i {}", i);
         }
@@ -127,7 +112,7 @@ async fn main() -> Result<()> {
     }
 
     tokio::spawn(async move {
-        while semaphore.available_permits() != max_conn {
+        while semaphore.available_permits() != args.max_conn {
             tokio::time::sleep(Duration::from_millis(100)).await;
         }
     }).await.unwrap_or(());
@@ -135,7 +120,7 @@ async fn main() -> Result<()> {
     let total_time = now.elapsed().as_millis();
     let success = stats.success.load(Ordering::Relaxed);
     let error = stats.error.load(Ordering::Relaxed);
-    let rps = count as u64 / (total_time as u64);
+    let rps = args.count as u64 / (total_time as u64);
 
     println!("total_time: {} success {} errors {} rps {}", total_time, success, error, rps);
 
